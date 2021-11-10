86bfad8ef17a89b4c32a466041e82abcf51d5bf3
rednikeeg <maks.schoolforvv@gmail.com>
2021-11-10 11:11:43 +0200

    Constructors extended.

--- src/main/java/com/rednikeeg/BigNumber.java
+++ src/main/java/com/rednikeeg/BigNumber.java
@@ -3,21 +3,57 @@ package com.rednikeeg;
 import java.util.Arrays;
 
 /**
- * The class represent integer, that can hold numbers from -1e100 + 1 to 1e100 - 1
- * @see LAMath
+ * The class represents integer, that can hold numbers from -1e100 + 1 to 1e100 - 1
+ *
  * @author Maksym "rednikeeg" Bohachov
+ * @see LAMath
  */
 public class BigNumber {
     private final int[] content;
     private static final int LENGTH = 100;
     private final boolean isNegative;
 
-    public BigNumber() {
-        throw new UnsupportedOperationException();
+    private BigNumber() {
+        content = new int[LENGTH];
+        Arrays.fill(content, 0);
+
+        isNegative = false;
     }
 
     public BigNumber(BigNumber num) {
-        throw new UnsupportedOperationException();
+        content = new int[LENGTH];
+        System.arraycopy(num.content, 0, content, 0, LENGTH);
+
+        isNegative = num.isNegative;
+    }
+
+    public BigNumber(String str) {
+        isNegative = str.toCharArray()[0] == '-';
+
+        if (isNegative) {
+            str = str.substring(1);
+        }
+
+        content = parse(str);
+    }
+
+    private int[] parse(String str) {
+        char[] chars = str.toCharArray();
+
+        int[] result = new int[LENGTH];
+        Arrays.fill(content, 0);
+
+        int length = chars.length;
+
+        for (int i = LENGTH - 1; i >= LENGTH - length - 1; i--) {
+            result[i] = parse(chars[i - length]);
+        }
+
+        return result;
+    }
+
+    private int parse(char ch) {
+        return ch - '0';
     }
 
     public int log10() {
